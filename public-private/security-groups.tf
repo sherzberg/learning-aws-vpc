d69db0bd9067a550da24dde9fbbe29d691da14ac
Spencer Herzberg <spencer.herzberg@gmail.com>
2015-06-09 20:34:40 -0500

    add egress for ALL traffic on web

--- public-private/security-groups.tf
+++ public-private/security-groups.tf
@@ -1,5 +1,4 @@
 
-/* Security group for the web */
 resource "aws_security_group" "web" {
   name = "web"
   description = "Security group for web that allows ssh and web traffic from internet"
@@ -23,6 +22,13 @@ resource "aws_security_group" "web" {
     from_port = 443
     to_port   = 443
     protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 
