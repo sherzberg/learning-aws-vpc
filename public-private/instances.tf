117d2a17e8afa5ea79c156404ebb8ce51b3d0108
Spencer Herzberg <spencer.herzberg@gmail.com>
2015-06-09 20:29:13 -0500

    add elastic ip for web instance

--- public-private/instances.tf
+++ public-private/instances.tf
@@ -1,14 +1,19 @@
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web_01" {
     ami = "${lookup(var.amis, var.region)}"
-    instance_type = "t1.micro"
+    instance_type = "${lookup(var.instance_type, var.region)}"
     subnet_id = "${aws_subnet.public.id}"
     key_name = "${aws_key_pair.dev.key_name}"
     vpc_security_group_ids = ["${aws_security_group.web.id}"]
 
     tags {
-        Name = "web"
+        Name = "web_01"
         Group = "dev"
     }
+}
+
+resource "aws_eip" "eip_01" {
+    vpc = true
+    instance = "${aws_instance.web_01.id}"
 }
 
---
+++ public-private/outputs.tf
@@ -0,0 +1,5 @@
+
+output "eip_01" {
+    value = "${aws_eip.eip_01.public_ip}"
+}
+
